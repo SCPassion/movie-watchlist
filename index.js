594ef60be08041cd6f4818b150f9af2a4df41774
SCPassion <141584825+SCPassion@users.noreply.github.com>
2024-12-24 16:07:39 +0100

    commit 9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const omdbApiKey = "b2fbbe57"
+import {formHTML} from "./htmlFunctions.js"
 
+const omdbApiKey = "b2fbbe57"
 const searchForm = document.getElementById("search-form")
 const searchResults = document.getElementById("search-results")
 const myMovieId = JSON.parse(localStorage.getItem("myMovieId"))
@@ -47,31 +48,4 @@ async function getSearchHtml(searchResultsArr) {
 
     const searchResultHTML = await Promise.all(searchResultPromises)
     return searchResultHTML.join("")
-}
-
-function formHTML(movieDetail) {
-    return `
-        <div class="movie-item">
-                    <div class="poster-container">
-                        <img src="${movieDetail.Poster}" alt="Movie poster" class="poster">
-                    </div>
-                    <div class="movie-description">
-                        <div class = "movie-title">
-                            <h3>${movieDetail.Title}<h3>
-                            <i class="fa-solid fa-star star"></i> 
-                            <p class="movie-rating">${movieDetail.imdbRating}</p>       
-                        </div>
-                        
-                        <div class="movie-meta">
-                            <p>${movieDetail.Runtime}</p>
-                            <p>${movieDetail.Genre}</p>
-                            <p class="add-watchlist" data-movieid="${movieDetail.imdbID}"><i class="fa-solid fa-circle-plus"></i>  Watchlist</p>
-                        </div>
-
-                        <div class="movie-plot">
-                            <article>${movieDetail.Plot}</article>
-                        </div>
-                    </div>
-                </div>
-                <hr class="divider">`
 }
